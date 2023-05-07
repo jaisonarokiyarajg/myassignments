270d1abecc044d59ef8390b02fc76feba2f5a94c
jaisonarokiyarajg <132812696+jaisonarokiyarajg@users.noreply.github.com>
2023-05-07 09:07:06 -0700

    Add files via upload

---
+++ Testing/src/main/java/week1/day1/car.java
@@ -0,0 +1,23 @@
+package week1.day1;
+
+public class car {
+	public void openDoor() {
+		System.out.println("openDoor");
+	}
+	public void fourwheels() {
+		System.out.println("fourwheels");
+	}
+	public void pressBreak() {
+		System.out.println("pressBreak");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	car car=new car();
+	car.openDoor();
+	car.fourwheels();
+	car.pressBreak();
+}
+	}
+
+
